1172e8a38003e8b34ccaae55cdaeb0efa6491665
Airam123456 <perseo_39@hotmail.es>
2021-09-28 10:03:38 +0200

    Empezando

---
+++ Tanda_2/Ejer_2.py
@@ -0,0 +1,21 @@
+
+import  csv
+import pandas as pd
+
+class GestorCSV:
+
+    # while num != 5:
+    #     print("Seleccione una opción escribiendo el número")
+    #     print("1: Generar fichero csv de olimpiadas")
+    #     print("2: Buscar deportista")
+    #     print("3: Buscar deportistas por deporte y olimpiada")
+    #     print("4: Añadir deportista")
+    #     print("5: Salir del programa")
+    #     num = int(input())
+
+    # datos = pd.read_csv("solo50.csv")
+    # olimpiadas = datos[['Year', 'Games', 'Season', 'City']].drop_duplicates()
+    # olimpiadas.to_csv('Olimpiadas.csv', index = False)
+
+
+
