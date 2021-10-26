b0b9bd11bced43f17b3c0d4873aee3cd7357a2d4
Airam123456 <perseo_39@hotmail.es>
2021-10-26 10:03:56 +0200

    seguimos, averiguar como crear las tablas

--- Tanda_3/BBDD.py
+++ Tanda_3/BBDD.py
@@ -1,48 +1,72 @@
 import mysql.connector
 import csv
+import os
 
 
-class ManejoConectores:
-
-    def conectarBBDD(self):
-
-        try:
-            conectordb = mysql.connector.connect(
-                host="localhost",
-                user="admin",
-                password="password",
-                database="olimpiadas"
-            )
-            print("Conexion con la base de datos")
-        except:
-            print("No se ha podido establecer la coneccion")
-
-    def borrarBBDD(self, conectordb):
-
-        mycursor = conectordb.cursor()
-        query = ["DROP TABLE IF EXISTS Participacion", "DROP TABLE IF EXISTS Evento", "DROP TABLE IF EXISTS Olimpiada",
-                 "DROP TABLE IF EXISTS Deporte", "DROP TABLE IF EXISTS Deportista", "DROP TABLE IF EXISTS Equipo"]
-        try:
-            mycursor.execute(query)
-            conectordb.commit()
-        except:
-            print("Fallo al borrar las tablas")
-
-        print(mycursor.rowcount, "record(s) deleted")
+def borrarBBDD(conectordb, cursor):
+    queryParticipacion = "DROP TABLE IF EXISTS Participacion"
+    queryEvento = "DROP TABLE IF EXISTS Evento"
+    queryOlimpiada = "DROP TABLE IF EXISTS Olimpiada"
+    queryDeporte = "DROP TABLE IF EXISTS Deporte"
+    queryDeportista = "DROP TABLE IF EXISTS Deportista"
+    queryEquipo = "DROP TABLE IF EXISTS Equipo"
+    try:
+        cursor.execute(queryParticipacion)
+        cursor.execute(queryEvento)
+        cursor.execute(queryOlimpiada)
+        cursor.execute(queryDeporte)
+        cursor.execute(queryDeportista)
+        cursor.execute(queryEquipo)
+        conectordb.commit()
+    except:
+        print("Fallo al borrar las tablas")
+    finally:
+        print(cursor.rowcount, "record(s) deleted")
         conectordb.close()
 
-    def crearTablas(self, conectordb, mycursor):
 
-        with open('olimpiadas.sql', 'r') as olimpFiles:
-            readTablas = olimpFiles.read()
-            tablas = mycursor.execute(readTablas, multi=True)
-            for row in tablas:
-                if row.with_rows:
-                    row.fetchall()
+def crearTablas(conectordb, cursor):
+    with open('olimpiadas.sql', 'r') as olimpFiles:
+        readTablas = olimpFiles.read()
+        tablas = cursor.execute(readTablas, multi=True)
+        for row in tablas:
+            if row.with_rows:
+                row.fetchall()
 
-        conectordb.commit()
+    conectordb.commit()
 
-    def crearBBDD(self):
+def crearBBDD():
+    try:
+        conectordb = mysql.connector.connect(
+            host="localhost",
+            user="admin",
+            password="password",
+            database="olimpiadas",
+            autocommit=False
+        )
+        cursor = conectordb.cursor()
+        print("Coneccion exitosa")
+    except:
+        print("Fallo en la coneccion")
+
+    try:
+        borrarBBDD(conectordb, cursor)
+        print("Base de datos vaciada correctamente.")
+        try:
+            crearTablas(conectordb, cursor)
+            print("Estructura de la base de datos creada correctamente.")
+        except:
+            print("No se ha podido crear la estructura de la base de datos.")
+    except:
+        print("No se ha podido borrar la base de datos.")
+
+    archivo = input("Introducir la direccion del archivo")
+
+    if not os.path.exists(archivo):
+        print("La dirección de archivo especificada no existe.")
+    else:
+        print("Accediendo al archivo csv...")
+
         dicOlimpiadas = {}
         dicEventos = {}
         dicParticipacion = {}
@@ -50,7 +74,7 @@ class ManejoConectores:
         dicDeportistas = {}
         dicEquipo = {}
 
-        with open('recortada.csv') as entrada:
+        with open(archivo) as entrada:
             reader = csv.reader(entrada, delimiter=',')
             idOlimpiada = 1
             idEvento = 1
@@ -58,11 +82,11 @@ class ManejoConectores:
             idDeporte = 1
             idDeportista = 1
             idEquipo = 1
-            firstRow = 0
+            primerRow = 0
 
             for row in reader:
-                if firstRow == 0:
-                    firstRow += 1
+                if primerRow == 0:
+                    primerRow += 1
                 else:
                     olimpiada = [row[8], row[9], row[10], row[11]]
                     if olimpiada is not dicOlimpiadas.values():
@@ -129,7 +153,12 @@ class ManejoConectores:
                         dicParticipacion[idParticipacion] = participacion
                         idParticipacion += 1
 
-        print(dicDeportes)
+            listaAux = []
+            for datos in dicOlimpiadas:
+                listaAux.append(list(dicOlimpiadas.get(datos)))
+            query = "insert into Olimpiada (id_olimpiada,nombre,anio,temporada,ciudad) values (%s, %s, %s, %s, %s)"
+            cursor.executemany(query, listaAux)
 
 
 crearBBDD()
+
